e406d139438e552da9d643509a524a391bf15939
skjhfds <beaver.hacker@gmail.com>
2022-08-01 12:06:34 +1000

    started load sensor things

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 #Spin servo @Dylan 
 #Take an image with PiCam @Calvin Medeira 
 #Read load sensor @pat 
+compensation = 0
+def readLS():
+    value=0 #fix this
+    return value
 #Calibrate load sensor @Harry 
+def calibrateLS():
+    compensation = readLS()
+
 #Read IMU @Kaelan
 
 #Process image
