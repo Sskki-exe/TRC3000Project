4391c71ae0cb1d9ec3e2450f63d16acfdd7dfd05
Calvin <cmed0005@student.monash.edu>
2022-08-04 09:20:26 +1000

    camera function

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 #Spin servo @Dylan 
 #Take an image with PiCam @Calvin Medeira 
+import picamera
+import time
+picture_number = 0
+def camera_picture():
+    camera = PiCamera()
+    camera.start_preview() # Display image
+    sleep(5)               # Give time for camera to sense light levels
+    camera.capture('/home/pi/Desktop/image%s.jpg', picture_number) # Save picture on desktop
+    picture_number += 1
+    camera.stop_preview()  # Close image
+
+
 #Read load sensor @pat 
 compensation = 0
 def readLS():
