afdc1e007a92ad54da4958ee71af98f3ee289e3c
Nils090 <dlarson090@gmail.com>
2022-08-04 12:13:19 +1000

    Moving imports

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
-#Spin servo @Dylan 
+import picamera
+import time
 import RPi.GPIO as GPIO
 from time import sleep
+from HX711 import *
+#Spin servo @Dylan 
+import smbus            #import SMBus module of I2C
 
-gpio_num=11 #Can change??
+servo_pin=11 #Can change??
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(gpio_num, GPIO.OUT)
-pwm=GPIO.PWM(gpio_num, 50) #Second parameter = freq.
+GPIO.setup(servo_pin, GPIO.OUT)
+pwm=GPIO.PWM(servo_pin, 50) #Second parameter = freq.
 pwm.start(0)
 
 def setAngle(angle):
     duty = angle / 18 + 3
-    GPIO.output(gpio_num, True)
+    GPIO.output(servo_pin, True)
     pwm.ChangeDutyCycle(duty)
     sleep(1)
-    GPIO.output(gpio_num, False)
+    GPIO.output(servo_pin, False)
     pwm.ChangeDutyCycle(duty)
 
 #Take an image with PiCam @Calvin Medeira 
-import picamera
-import time
 picture_number = 0
 def camera_picture():
     camera = PiCamera()
@@ -34,7 +36,6 @@ def camera_picture():
 
 # must do "pip install hx711-rpi-py==1.57.0" first 
 # rpi gpio has to be also installed
-from HX711 import *
 
 hx = SimpleHX711(2, 3, -370, -367471)
 hx.zero()
@@ -55,8 +56,6 @@ def calibrateLS():
 # https://www.electronicwings.com/raspberry-pi/mpu6050-accelerometergyroscope-interfacing-with-raspberry-pi
 # https://invensense.tdk.com/wp-content/uploads/2015/02/MPU-6000-Datasheet1.pdf
 
-import smbus            #import SMBus module of I2C
-from time import sleep          #import
 
 #some MPU6050 Registers and their Address
 PWR_MGMT_1   = 0x6B
