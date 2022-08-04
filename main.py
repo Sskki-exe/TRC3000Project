92dcdd19498ecf1fe4077ff9f17b9a9e515e7b24
patrickhayhoe <phay0005@student.monash.edu>
2022-08-04 11:11:38 +1000

    readls

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
-import picamera
-import time
+#Spin servo @Dylan 
 import RPi.GPIO as GPIO
 from time import sleep
-from HX711 import *
-import smbus            #import SMBus module of I2C
-#Spin servo @Dylan 
 
-servo_pin=11 #Can change??
+gpio_num=11 #Can change??
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(servo_pin, GPIO.OUT)
-pwm=GPIO.PWM(servo_pin, 50) #Second parameter = freq.
+GPIO.setup(gpio_num, GPIO.OUT)
+pwm=GPIO.PWM(gpio_num, 50) #Second parameter = freq.
 pwm.start(0)
 
 def setAngle(angle):
     duty = angle / 18 + 3
-    GPIO.output(servo_pin, True)
+    GPIO.output(gpio_num, True)
     pwm.ChangeDutyCycle(duty)
     sleep(1)
-    GPIO.output(servo_pin, False)
+    GPIO.output(gpio_num, False)
     pwm.ChangeDutyCycle(duty)
 
 #Take an image with PiCam @Calvin Medeira 
+import picamera
+import time
 picture_number = 0
 def camera_picture():
     camera = PiCamera()
@@ -37,16 +35,14 @@ def camera_picture():
 # must do "pip install hx711-rpi-py==1.57.0" first 
 # rpi gpio has to be also installed
 
-hx = SimpleHX711(2, 3, -370, -367471)
-hx.zero()
-while True:
-  print(hx.weight(35))
 
 
 compensation = 0
 def readLS():
-    value=0 #fix this
-    return value
+from HX711 import *
+hx = SimpleHX711(2, 3, -370, -367471)
+hx.zero()
+  return (hx.weight(35))
 #Calibrate load sensor @Harry 
 def calibrateLS():
     compensation = readLS()
@@ -56,6 +52,8 @@ def calibrateLS():
 # https://www.electronicwings.com/raspberry-pi/mpu6050-accelerometergyroscope-interfacing-with-raspberry-pi
 # https://invensense.tdk.com/wp-content/uploads/2015/02/MPU-6000-Datasheet1.pdf
 
+import smbus            #import SMBus module of I2C
+from time import sleep          #import
 
 #some MPU6050 Registers and their Address
 PWR_MGMT_1   = 0x6B
