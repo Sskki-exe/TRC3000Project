f3a831a9d6676a907fa320644135c8a12550bf82
skjhfds <beaver.hacker@gmail.com>
2022-09-22 02:01:00 +1000

    finished html ig

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from asyncio.windows_events import NULL
+from cmath import sqrt
 from flask import Flask, request, render_template, jsonify
 import main
 
@@ -14,6 +15,19 @@ def stuff():
     ay=main.MPU_getValue('aY')
     az=main.MPU_getValue('aZ')
     return jsonify(mass=mass,tx=tx,ty=ty,ax=ax,ay=ay,az=az)
+
+@app.route('/getdataB', methods= ['GET'])
+def stuffB():
+    speed = 0 #fastest speed
+    tx=main.MPU_getValue('wX')
+    ty=main.MPU_getValue('wY')
+    ts=sqrt(tx^2 + ty^2)
+
+    ax=main.MPU_getValue('aX')
+    ay=main.MPU_getValue('aY')
+    aS=sqrt(ax^2 + ay^2)
+
+    return jsonify(speed=speed,tx=tx,ty=ty,ts=ts,ax=ax,ay=ay,aS=aS)
 
 @app.route('/partA', methods=["POST"])
 def partA():
@@ -33,10 +47,24 @@ def partA():
 
 @app.route('/partB', methods=["POST"])
 def partB():
+    if request.method == "POST":
+        btn = request.form['btn']
+        if btn == "Calibrate":
+            pass #calibration function goes here
     return render_template('partB.html')
 
 @app.route('/partC', methods=["POST"])
 def partC():
+    if request.method == "POST":
+        btn = request.form['btn']
+        if btn == "Tare Load Cell":
+            main.tareLS()
+        if btn == "Calibrate LC":
+            main.calibrateLS(request.form['amount'])
+        if btn == "Calibrate IMU":
+            main.MPU_calibrate()
+        if btn == "Program Start":
+            pass #function to start program here
     return render_template('partC.html')
 
 if __name__ == "__main__":
