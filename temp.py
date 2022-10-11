5c3ddda70638f0a79ecd3a1d6c57312f9dd84efc
skjhfds <beaver.hacker@gmail.com>
2022-10-12 03:07:23 +1100

    continuous data

--- temp.py
+++ temp.py
@@ -1,15 +1,26 @@
 import random
+import time
 from flask import Flask, request, render_template, jsonify
 
 app = Flask(__name__)
+
+dataSize = 50
+dataLabels = []
+dataValues = []
+def genData():
+    dataLabels.append(time.strftime("%H:%M:%S", time.localtime()))
+    dataValues.append(random.random()*100)
+    while len(dataLabels) > dataSize:
+        dataLabels.pop(0)    
+    while len(dataValues) > dataSize:
+        dataValues.pop(0)
 
 @app.route('/getdataC', methods= ['GET'])
 def stuffC():
     mass = int(random.random()*10)
     fl = int(random.random()*10)
-    labels = ["a","b","c","d","e"]
-    values = random.sample(range(1,10),5)
-    return jsonify(m=mass,fl=fl,l=labels,v=values)
+    genData()
+    return jsonify(m=mass,fl=fl,l=dataLabels,v=dataValues)
 
 @app.route('/', methods=["GET","POST"])
 def home():
