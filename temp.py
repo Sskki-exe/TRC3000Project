7184bffcd4dd598560eb15d88c8108aa9ec358c2
skjhfds <beaver.hacker@gmail.com>
2022-10-12 02:13:13 +1100

    random data test

--- temp.py
+++ temp.py
@@ -1,19 +1,21 @@
-from random import random
+import random
 from flask import Flask, request, render_template, jsonify
 
 app = Flask(__name__)
 
 @app.route('/getdataC', methods= ['GET'])
 def stuffC():
-    mass = random()
-    fl = random()
-    return jsonify(m=mass,fl=fl)
+    mass = random.random()
+    fl = random.random()
+    labels = ["a","b","c","d","e"]
+    values = random.sample(range(1,10),5)
+    return jsonify(m=mass,fl=fl,l=labels,v=values)
 
 @app.route('/', methods=["GET","POST"])
 def home():
     if request.method == "POST":
         pass
-    return render_template('partC.html', labels = ["a","b","c"], values = [1,2,3])
+    return render_template('partC.html')
 
 if __name__=="__main__":
     app.run(host="0.0.0.0", port=80)
